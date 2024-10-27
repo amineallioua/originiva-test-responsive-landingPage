b6bd461b4900cd8e42dac3d9593966025a1217cb
amineallioua <122396863+amineallioua@users.noreply.github.com>
2024-10-27 16:21:11 +0100

    Add files via upload

---
+++ default.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+    <div
+      class="w-full pt-[70px] h-20 absolute flex justify-between items-center z-50"
+    >
+      <div class="flex items-center pl-[150px]">
+        <span
+          class="text-[34px] p-0 text-white sm:text-[#A06056] font-bold font-merriweather"
+          >antic
+        </span>
+        <ul
+          class="gap-[24px] ml-[32px] text-[18px] mt-1 font-[400] text-white xl:text-[#706458] lg:flex hidden font-Varta"
+        >
+          <li>Products</li>
+          <li>Rooms</li>
+          <li>Services</li>
+          <li>Inspirations</li>
+        </ul>
+      </div>
+
+      <button
+        @click="togglesidebar"
+        :class="[
+          'lg:hidden border-2 absolute text-[20px] font-[700] text-white  border-white   rounded-[50%]  h-[50px] w-[50px] right-10 transition-all duration-500 ease-in-out ',
+          isopen ? 'hidden' : 'block'
+        ]"
+      >
+        -
+      </button>
+
+      <nuxt-img
+        class="mr-10 lg:block hidden"
+        src="Contact.svg"
+        alt="My Image"
+        width="50"
+        height="50"
+        quality="85"
+        blur="10"
+      />
+    </div>
+    <div
+      :class="[
+        ' md:w-[40%] h-full w-[70%]  fixed top-0 b right-0 z-50 bg-[#A06056] opacity-80  overflow-hidden justify-between px-10 pt-10 transition-all duration-500 ease-in-out',
+        isopen ? 'right-0' : 'md:right-[-40%] right-[-70%] '
+      ]"
+    >
+      <button
+        @click="togglesidebar"
+        class="right-10 top-5 absolute border-2 text-[20px] font-[700] text-white border-white rounded-[50%] h-[50px] w-[50px]"
+      >
+        >
+      </button>
+      <ul class="relative right-0 ml-5 mt-[100px] text-[25px] text-white">
+        <li class="mb-10">Products</li>
+        <li class="mb-10">Rooms</li>
+        <li class="mb-10">Services</li>
+        <li class="mb-10">Inspirations</li>
+      </ul>
+    </div>
+    <slot />
+  </div>
+</template>
+<script setup>
+let isopen = ref(false);
+const togglesidebar = () => {
+  isopen.value = !isopen.value;
+};
+</script>
