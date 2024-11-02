cc6bac8a1ce03ecd580e0a2695fa7a57cb17f03e
amineallioua <amineallioua123@gmail.com>
2024-11-02 13:48:53 +0100

    hb

--- components/photos.vue
+++ components/photos.vue
@@ -1,8 +1,8 @@
 <template>
     <div
-      class="lg:w-[458px] overfloww-hidden   lg:h-[550px] md:w-[308px] md:h-[400px] w-[145px] lg:m-0 mb-10 ml-5 h-[175px]"
+      class="lg:w-[458px] overfloww-hidden relative object-cover  lg:h-[550px] md:w-[308px] md:h-[400px] w-[145px] lg:m-0 mb-10 ml-5 h-[175px]"
     >
-      <nuxt-img :src="image"       :placeholder="500"     class="w-[150%] absolute bottom-[-100px] object-cover h-[150%]  "></nuxt-img>
+      <nuxt-img :src="image"       :placeholder="500"     class="lg:w-[150%] h-full w-full lg:absolute bottom-[-100px] object-cover lg:h-[150%]  "></nuxt-img>
   
       <h1 class="mt-2 text-[15px] font-[400] text-[#121212]">{{ name }}</h1>
     </div>
